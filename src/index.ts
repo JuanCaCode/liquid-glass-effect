43181a24c39db23fa9492d93c4badd5e32f9de98
Juan Camilo Campo T <juanccampo95@gmail.com>
2025-06-15 21:10:22 -0500

    updates(package.json)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { LiquidGlassProvider } from "./providers/LiquidGlassProvider";
 export { GlassSVGDefs } from "./utils/svgDefs";
-import ".styles/main_liquid_glass.css";
+import "./styles/main_liquid_glass.css";
